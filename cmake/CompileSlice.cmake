dbedee287aa0626b489b20bf7939d5e7ad6c3b1e
Matteo Settenvini <matteo@member.fsf.org>
2020-02-18 21:53:17 +0100

    Update CompileSlice.cmake

--- cmake/CompileSlice.cmake
+++ cmake/CompileSlice.cmake
@@ -8,35 +8,27 @@
 # output_file - generated source output file name
 
 function(CompileSlice slice2bin_params slice_include_paths ice_file output_dir output_file)
-	set(ice_children "")
-	if(EXISTS ${output_dir}/${ice_name}.d)
-		file(STRINGS ${output_dir}/${ice_name}.d ice_children)
-		list(LENGTH ${ice_children} ice_children_len)
-		foreach(child IN LISTS ${ice_children} RANGE 1 ${ice_children_len} 1)
-			string(REPLACE "\"" "" child ${child})
-		endforeach()
-	endif()
+  set(depfile "${output_dir}/${ice_name}.d"
+  if(EXISTS "${depfile}")
+    file(STRINGS "${depfile}" ice_children)
+    list(POP_FRONT ice_children)
+    foreach(child IN LISTS ice_children)
+      string(REPLACE "\"" "" child "${child}")
+    endforeach()
+  endif()
 
-	get_filename_component(ice_name ${ice_file} NAME_WE)
-	add_custom_command(
-		OUTPUT 
-			${output_dir}/${ice_name}.d 
-			${output_file}
-		COMMAND ${SLICE2_BIN}
-		ARGS 
-			${slice_include_paths} 
-			--depend 
-			--depend-file ${ice_name}.d
-			${ice_file}
-		COMMAND ${SLICE2_BIN}
-		ARGS 
-			${slice_include_paths} 
-			${slice2bin_params} 
-			${ice_file}
-		COMMENT "Generating sources for ${ice_file}..."
-		BYPRODUCTS ${output_dir}/${ice_name}.d ${output_file}
-		WORKING_DIRECTORY ${output_dir}
-		MAIN_DEPENDENCY ${ice_file}
-		DEPENDS ${SLICE2_BIN}
-	)
+  get_filename_component(ice_name "${ice_file}" NAME_WE)
+  add_custom_command(OUTPUT "${output_file}"
+    COMMAND ${SLICE2_BIN} ${slice_include_paths} 
+                          --depend
+                          --depend-file "${depfile}"
+                          "${ice_file}"
+    COMMAND ${SLICE2_BIN} ${slice_include_paths} 
+                          ${slice2bin_params} 
+                          "${ice_file}"
+    COMMENT "Generating sources for ${ice_file}..."
+    DEPENDS "${ice_file}"
+    BYPRODUCTS "${depfile}"
+    WORKING_DIRECTORY "${output_dir}"
+    VERBATIM)
 endfunction()
